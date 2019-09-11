cb457f9f2f601bf7596ad82f38ae3f9651366eb1
AugF <wangyunpan@outlook.com>
2019-09-11 12:45:53 +0800

    remove old version

---
+++ gcn/data_utils.py
@@ -0,0 +1,23 @@
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+
+def load_data(dataset_str):
+    import pickle as pkl
+    import sys
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    return x, y, tx, ty, allx, ally, graph   # graph 邻接矩阵
---
+++ gcn/vis_utils.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# 图的可视化
+
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# 1. import data
+n, c = 34, 4
+labels = np.random.randint(0,4, size=(n,))
+pos = np.random.uniform(-1,1, size=(n,2))
+
+# 2. draw point
+
+g = lambda li, idx: pos[li, ][:, idx]
+f = lambda x: np.argwhere(labels==x).ravel().tolist()
+
+plt.scatter(g(f(0),0), g(f(0), 1), color='r', s=50)
+plt.scatter(g(f(1),0), g(f(1), 1), color='g', s=50)
+plt.scatter(g(f(2),0), g(f(2), 1), color='b', s=50)
+plt.scatter(g(f(3),0), g(f(3), 1), color='c', s=50)
+
+# 3. draw edges
+edges = nx.karate_club_graph().edges
+for edge in edges:
+    point1 = pos[edge[0]]
+    point2 = pos[edge[1]]
+    plt.plot([point1[0], point2[0]], [point1[1], point2[1]], color='k')
+
+plt.show()
