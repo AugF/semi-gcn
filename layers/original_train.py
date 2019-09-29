8f496f14d66a6582f141ef8fd34a78582ec0074f
AugF <wangyunpan@outlook.com>
2019-09-30 07:55:42 +0800

    pubmed, citeseer test successful!!!

--- layers/original_train.py
+++ layers/original_train.py
@@ -29,28 +29,30 @@ if __name__ == '__main__':
         # train step
         train_loss, train_acc = model.one_train()
         # print("model loss", train_loss)
-        print("before adam checked weight_outputs",  model.weight_outputs)
-        print("before adam checked weight_hidden",  model.weight_hidden)
+        # print("before adam checked weight_outputs",  model.weight_outputs)
+        # print("before adam checked weight_hidden",  model.weight_hidden)
 
         model.one_update()
         # print("model outputs", model.outputs)
         # print("model weight_outputs", model.grad_weight_outputs)
         # print("model weight_hidden", model.grad_weight_hidden)
-        print("after adam checked weight_outputs",  model.weight_outputs)
-        print("after adam checked weight_hidden",  model.weight_hidden)
-        break
-    #
-    #     # val step
-    #     val_loss, val_acc = model.evaluate()
-    #     print("iteration: {}, train_loss: {}, train_acc: {}, val_loss: {}, val_acc: {}".
-    #           format(i, train_loss, train_acc, val_loss, val_acc))
-    #     cost_val.append(val_loss)
-    #
-    #     if i > early_stopping and cost_val[-1] > np.mean(cost_val[-(early_stopping + 1): -1]):
-    #         print("early stopping ! ")
-    #         break
-    #
-    # # test step
-    # test_loss, test_acc = model.test()
-    # print("start test, the loss: {}, the acc: {}".format(test_loss, test_acc))
+        # print("after adam checked weight_outputs",  model.weight_outputs)
+        # print("after adam checked weight_hidden",  model.weight_hidden)
+        # break
+
+        # val step
+        val_loss, val_acc = model.evaluate()
+        print("iteration: {}, train_loss: {}, train_acc: {}, val_loss: {}, val_acc: {}".
+              format(i, train_loss, train_acc, val_loss, val_acc))
+        cost_val.append(val_loss)
+
+        if i > early_stopping and cost_val[-1] > np.mean(cost_val[-(early_stopping + 1): -1]):
+            print("early stopping ! ")
+            break
+
+    # test step
+    test_loss, test_acc = model.test()
+    print("start test, the loss: {}, the acc: {}".format(test_loss, test_acc))
+    save_weight(data_str + "train_weight_outputs", model.weight_outputs)
+    save_weight(data_str + "train_weight_hidden", model.weight_hidden)
 
